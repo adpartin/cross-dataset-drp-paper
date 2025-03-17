76261c9c6b4cec3dc065c549d55f6cbd5aafeb67
Alex Partin <25892264+adpartin@users.noreply.github.com>
2025-03-17 16:29:09 -0700

    setup_improve.sh

--- setup_improve.sh
+++ setup_improve.sh
@@ -7,22 +7,21 @@
 this_path=$PWD
 echo "This path: $this_path"
 
-# Clone IMPROVE lib (if needed)
+## Clone IMPROVE lib (if needed)
+##cd ../
+#improve_lib_path=$PWD/IMPROVE
+#improve_branch="develop"
+#if [ -d $improve_lib_path ]; then
+#  echo "IMPROVE repo exists in ${improve_lib_path}"
+#else
+#    git clone https://github.com/JDACS4C-IMPROVE/IMPROVE
+#fi
+#cd IMPROVE
+#git checkout $improve_branch
+##cd $this_path
 #cd ../
-improve_lib_path=$PWD/IMPROVE
-improve_branch="develop"
-if [ -d $improve_lib_path ]; then
-  echo "IMPROVE repo exists in ${improve_lib_path}"
-else
-    git clone https://github.com/JDACS4C-IMPROVE/IMPROVE
-fi
-cd IMPROVE
-git checkout $improve_branch
-#cd $this_path
-cd ../
 
 # Env var PYTHOPATH
 export PYTHONPATH=$PYTHONPATH:$improve_lib_path
 
-echo
 echo "PYTHONPATH: $PYTHONPATH"
